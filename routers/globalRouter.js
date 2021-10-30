841b75de38cb96aecd46dbf9c0528acc8e57a84d
정우민 <jeong-umin@jeong-umin-ui-MacBookAir.local>
2021-10-30 13:47:16 +0900

    JUM

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -7,4 +7,27 @@ router.get("/",(req,res,next)=>{
     res.render("screens/main");
 });
 
+router.get("/about",(req,res,next)=>{
+    res.render("screens/about");
+});
+
+router.get("/review",(req,res,next)=>{
+    res.render("screens/review");
+});
+
+router.get("/pet",(req,res,next)=>{
+    res.render("screens/pet");
+});
+
+router.get("/inquiry",(req,res,next)=>{
+    res.render("screens/inquiry");
+});
+
+router.get("/center",(req,res,next)=>{
+    res.render("screens/center");
+});
+
+router.get("/beauty",(req,res,next)=>{
+    res.render("screens/beauty");
+});
 module.exports=router;
