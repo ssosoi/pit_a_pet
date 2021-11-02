582cf5b4313e57a4941fcb4c07a71c8a3f567ae4
정우민 <jeong-umin@jeong-umin-ui-MacBookAir.local>
2021-11-03 07:36:11 +0900

    up

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -7,27 +7,55 @@ router.get("/",(req,res,next)=>{
     res.render("screens/main");
 });
 
+
+
 router.get("/about",(req,res,next)=>{
     res.render("screens/about");
 });
+router.get("/notice",(req,res,next)=>{
+    res.render("screens/notice");
+});
+router.get("/story",(req,res,next)=>{
+    res.render("screens/story");
+});
+
+router.get("/order",(req,res,next)=>{
+    res.render("screens/order");
+});
+router.get("/loc",(req,res,next)=>{
+    res.render("screens/loc");
+});
+
+
+
 
 router.get("/review",(req,res,next)=>{
     res.render("screens/review");
 });
 
+
+
 router.get("/pet",(req,res,next)=>{
     res.render("screens/pet");
 });
+
+
 
 router.get("/inquiry",(req,res,next)=>{
     res.render("screens/inquiry");
 });
 
+
+
 router.get("/center",(req,res,next)=>{
     res.render("screens/center");
 });
 
+
+
 router.get("/beauty",(req,res,next)=>{
     res.render("screens/beauty");
 });
+
+
 module.exports=router;
